d25648cb6bded922ae7f5eb98be20d48e37f0fce
anonlegionoke <anonlegionoke@protonmail.com>
2024-01-23 10:07:27 +0530

    add service for database and storage

--- 11blogapp/src/appwrite/config.js
+++ 11blogapp/src/appwrite/config.js
@@ -1,11 +1,104 @@
+import { Client, Databases, ID, Storage, Query} from "appwrite";
 import conf from "../conf/conf"
 
+export class Service{
+    client = new Client()
+    account;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async getPost(slug) {
+        try {
+           return await this.databases.getDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
+        } catch (error) {
+            console.log("Appwrite service :: getPost() ::",error)
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(conf.appwriteDatabaseId, conf.appwriteCollectionId, queries)
+        } catch (error) {
+            console.log( "Appwrite service :: getPosts() ::" ,error)
+            return false
+        }
+
+    }
+
+    /* we use slug for the ID.unique() */
+
+    async createPost({title, slug, content, featuredImage, status, userId}) {
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+                title, content, featuredImage, status, userId
+            })
+        } catch (error) {
+            console.log( "Appwrite service :: createPost() ::" ,error)
+            return false
+        }
+
+    }
+
+    async updatePost(slug, {title, slug, content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+                title, content, featuredImage, status
+            })
+        } catch (error) {
+            console.log( "Appwrite service :: updatePost() ::" ,error)
+            return false
+        }
+
+    }
+   
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
+            return true;
+        } catch (error) {
+            console.log( "Appwrite service :: deletePost() ::" ,error)
+            return false
+        }
+
+    }
+    
+
+    // BUILDING STORAGE SERVICE
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(conf.appwriteBucketId, ID.unique(), file)
+        } catch (error) {
+            console.log( "Appwrite service :: uploadFile() ::" ,error)
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFileFile(conf.appwriteBucketId, fileId)
+            return true;
+        } catch (error) {
+            console.log( "Appwrite service :: deleteFile() ::" ,error)
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(conf.appwriteBucketId, fileId).href
+    }
+
+}
 
 
-import { Client, Databases, ID, Storage, Query} from "appwrite";
+const service = new Service()
 
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('<PROJECT_ID>');
-
-const databases = new Databases(client);
+export default service
